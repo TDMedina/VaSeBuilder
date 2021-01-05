9bdf61e3c338cbda88c8ed4be82a6981358d4525
tdmedina <tylerdanmedina@gmail.com>
2021-01-05 15:21:36 +0000

    Added parameter and module load for Python version, fixed typos.

--- VaSeUtils/ygor_protocol.sh
+++ VaSeUtils/ygor_protocol.sh
@@ -1,15 +1,21 @@
+#Parameter mapping
+
+#string python3Version
+
 #string ygorLocation
 #string vbGoldStandard
 #string vbVcfCalls
 #string ygorOutdir
 #string project
 
+ml "${python3Version}"
+
 echo "Compare VaSeBuilder variant calls to gold standard variant calls"
-python "${ygorLocation}" -1 "${vbGoldStandard}" -2 "${$vbVcfCalls}" -o "${ygorOutdir}" -op "${project}"
+python "${ygorLocation}" -1 "${vbGoldStandard}" -2 "${vbVcfCalls}" -o "${ygorOutdir}" -op "${project}"
 echo "Comparison to gold standard complete"
 
 #SHORT EXPLANATION ON THE DEFINED PARAMETERS
-#ygorLocation = Path to the location of ygor.py tha will perform the comparison
+#ygorLocation = Path to the location of ygor.py that will perform the comparison
 #vbGoldStandard = Path to VCF file containing the original calls of all variants added to the VaSeBuilder sample
 #vbVcfCalls = Path to the VCF file for calls on the VaSeBuilder sample produced by the NGS_DNA pipeline
 #ygorOutDir = Path to the directory where the comparison output files should be written to
